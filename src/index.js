70ee646323840de8821089049a03616ac362b881
Lightening Gibson <lighteningibson@gmail.com>
2022-12-20 18:05:01 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+
 
 function Home() {
   return (
@@ -9,8 +11,94 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+const linkStyles ={
+  dispay: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+
+}
+
+function NavBar() { 
+  return (
+    <div>
+      <NavLink to="/"
+                exact
+                style={linkStyles}
+                activeStyle={{background : "darkblue",}}
+                >
+                  Home 
+      </NavLink>
+      <NavLink
+                to="/about"
+                exact
+                style={linkStyles}
+                activeStyle={{background: "darkblue",}}
+                >
+                    About
+      </NavLink>
+      <NavLink
+              to="/login"
+              exact
+              style={linkStyles}
+              activeStyle={{background: "darkblue",}}  
+              >
+                    Login
+      </NavLink>
+    </div>
+
+  );
+}
+
+
+function App() {
+  return ( <div>
+      <NavBar />
+    <Switch>
+    <Route exact path="/about">
+      <About />
+    </Route>
+    <Route exact path="/login">
+      <Login />
+    </Route>
+    <Route exact path="/" >
+      <Home />
+    </Route>
+    </Switch>
+          </div>
+  );
+
+}
+
+
+
+ReactDOM.render(
+<BrowserRouter> <App /> </BrowserRouter>, document.getElementById("root"));
